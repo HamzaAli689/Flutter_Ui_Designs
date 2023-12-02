9d1bc589493836854aee9ff996586af9735a0801
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2023-12-02 12:10:43 +0500

    new wallet app

---
+++ lib/screens/Home_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class Home_Page extends StatefulWidget {
+  const Home_Page({super.key});
+
+  @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "My",
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Text(
+                          "Wallet",
+                          style: TextStyle(
+                            fontSize: 28,
+                            //fontWeight: FontWeight.bold,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade400,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Icon(
+                        Icons.notifications
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              Container(
+                
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/my_cards.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class My_Cards extends StatelessWidget {
+  final double balance;
+  final int cardnumber;
+  final int expiredMonth;
+  final int expiredYear;
+  final String cardType;
+
+  const My_Cards(
+      {super.key,
+      required this.balance,
+      required this.cardnumber,
+      required this.expiredMonth,
+      required this.expiredYear,
+      required this.cardType});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+      child: Container(
+        width: 300,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+                begin: AlignmentDirectional.topStart,
+                end: AlignmentDirectional.bottomEnd,
+                colors: [
+                  Colors.white.withOpacity(0.03),
+                  Colors.white.withOpacity(0.03),
+                ])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Balance",
+                  style: TextStyle(color: Colors.white),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Image(
+                      image: AssetImage(cardType),
+                      height: 50,
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 10,),
+            Text("\$" + balance.toString(),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            SizedBox(height: 30,),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(cardnumber.toString(),
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              ),
+              Text(expiredMonth.toString() + "/" + expiredYear.toString())
+            ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
