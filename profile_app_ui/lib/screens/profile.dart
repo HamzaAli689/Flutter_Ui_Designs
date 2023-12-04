fca2ea6421c112f03f2f9fafd32200e7681f3fd0
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2023-12-04 23:58:33 +0500

    add white screen

--- profile_app_ui/lib/screens/profile.dart
+++ profile_app_ui/lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import '../widgets/circle_avatar.dart';
 
 class Profile_Screen extends StatefulWidget {
@@ -35,7 +36,26 @@ class _Profile_ScreenState extends State<Profile_Screen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(height: 30),
-              Center(child: Circle_Avatar()),
+                  Center(child: Stack(
+                    children: [
+                      Circle_Avatar(),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              color: Colors.yellowAccent
+                          ),
+                          child: Icon(
+                              LineAwesomeIcons.alternate_pencil
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
               SizedBox(
                 height: 10,
               ),
@@ -60,41 +80,100 @@ class _Profile_ScreenState extends State<Profile_Screen> {
                   width: 200,
                   child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.yellow),
+                          backgroundColor: Colors.yellowAccent),
                       onPressed: () {},
                       child: Text(
                         "Edit Profile",
                         style: TextStyle(
                             color: Colors.black, fontWeight: FontWeight.w300),
                       ))),
-              SizedBox(height: 40,),
-              ListTile(
-                leading: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: Colors.black12
-                  ),
-                  child: Icon(Icons.settings,color: Colors.blue),
-                ),
-                title: Text("Settings",style: TextStyle(
-                  color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 18
-                )),
-                trailing: Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: Colors.black12
-                  ),
-                  child: Icon(Icons.arrow_forward_ios,color: Colors.grey),
-                ),
-              )
+              SizedBox(
+                height: 40,
+              ),
+              ProfileMenuWidget(
+                title: 'Settings',
+                icon: LineAwesomeIcons.cog,
+                onpress: () {},
+                textColor: Colors.black,
+              ),
+              SizedBox(height: 7),
+              ProfileMenuWidget(
+                title: 'Billing Details',
+                icon: LineAwesomeIcons.wallet,
+                onpress: () {},
+                textColor: Colors.black,
+              ),
+              SizedBox(height: 7),
+              ProfileMenuWidget(
+                title: 'User Management',
+                icon: LineAwesomeIcons.user_check,
+                onpress: () {},
+                textColor: Colors.black,
+              ),
+              Divider(),
+              SizedBox(height: 15),
+              ProfileMenuWidget(
+                title: 'Information',
+                icon: LineAwesomeIcons.info,
+                onpress: () {},
+                textColor: Colors.black,
+              ),
+              SizedBox(height: 7),
+              ProfileMenuWidget(
+                title: 'Logout',
+                icon: LineAwesomeIcons.alternate_sign_out,
+                onpress: () {},
+                textColor: Colors.red,
+              ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileMenuWidget extends StatelessWidget {
+  const ProfileMenuWidget({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onpress,
+    required this.textColor,
+  });
+
+  final String title;
+  final IconData icon;
+  final VoidCallback onpress;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: ListTile(
+        leading: Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100), color: Colors.black12),
+          child: Icon(icon, color: Colors.blue),
+        ),
+        title: Text(title,
+            style: TextStyle(
+                color: textColor,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.normal,
+                fontSize: 18)),
+        trailing: Container(
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100), color: Colors.black12),
+          child: Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.grey,
+            size: 18.0,
           ),
         ),
       ),
