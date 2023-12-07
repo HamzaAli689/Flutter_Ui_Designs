8c08c1256a09c00196201175dfcbd0e82d1e9c5c
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2023-12-07 18:30:00 +0500

    working on 2nd screen

--- login_app_ui/lib/login_screen.dart
+++ login_app_ui/lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_app_ui/get_strated_screen.dart';
 
 class Login_Screen extends StatelessWidget {
   const Login_Screen({super.key});
@@ -8,22 +9,102 @@ class Login_Screen extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: Colors.lightBlueAccent,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          title: Text("Login",style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          )),
+          title: Text("Login",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold)),
           centerTitle: true,
-          leading: Icon(Icons.arrow_back_ios_new,
-            color: Colors.black,),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new),
+            color: Colors.black,
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GetStarted_Screen()));
+            },
+          ),
         ),
-        body: Column(
-          children: [
-            Image.asset("images/login.png")
-          ],
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Image.asset("images/login2.png", height: 250),
+                Text(
+                  "WELCOME BACK",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 70),
+                  child: Text(
+                    "More it Work, More it Right",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                Text(
+                  "More it Fast ",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                    decoration: InputDecoration(
+                        hintText: "E-mail",hintStyle: TextStyle(
+                      color: Colors.white
+                    ),
+                        fillColor: Colors.black,
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30),),
+                        filled: true,
+                        prefixIcon: Icon(Icons.email, color: Colors.yellowAccent),
+                    )
+                  ),
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: TextFormField(obscureText: true,
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      decoration: InputDecoration(
+                        hintText: "Password",hintStyle: TextStyle(
+                          color: Colors.white
+                      ),
+                        fillColor: Colors.black,
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(30),),
+                        filled: true,
+                        prefixIcon: Icon(Icons.lock, color: Colors.yellowAccent),
+                      )
+                  ),
+                ),
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 200),
+                      child: Text("Forget Password ?",style: TextStyle(
+                        color: Colors.yellowAccent,
+                        fontWeight: FontWeight.w400,
+                      ),),
+                    ),
+                  ],
+                ),
+                
+              ],
+            ),
+          ),
         ),
       ),
     );
