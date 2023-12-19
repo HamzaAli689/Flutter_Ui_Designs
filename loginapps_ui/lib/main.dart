c241db03aaef590fd1aed59c10afef75602d83e7
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2023-12-19 16:49:54 +0500

    run login 7 screen

--- loginapps_ui/lib/main.dart
+++ loginapps_ui/lib/main.dart
@@ -8,7 +8,7 @@ import 'logins/login2.dart';
 import 'logins/login3.dart';
 import 'logins/login4.dart';
 import 'logins/login5.dart';
-import 'logins/login6.dart';
+import 'logins/login7.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,12 +17,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginSixPage(),
+      home: LoginSevenPage(),
     );
   }
 }
