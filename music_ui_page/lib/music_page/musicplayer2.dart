63ecd6a9841c84d1b86ab15b0596f71824b635ee
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2023-12-29 17:57:40 +0500

    add more widgets screen

--- music_ui_page/lib/music_page/musicplayer2.dart
+++ music_ui_page/lib/music_page/musicplayer2.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
-import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
+
 import 'package:flutter_ui_challenges/src/pages/invitation/inlanding.dart';
 
 class MusicPlayerTwoPage extends StatelessWidget {
