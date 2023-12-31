312044a7018e7c6d0becce11984fa2b328fe9b2c
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2023-12-31 21:26:31 +0500

    add 2nd page

--- food_ui_pages/lib/avocado.dart
+++ food_ui_pages/lib/avocado.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:food_ui_pages/widgets/assets.dart';
 
 class AvocadoPage extends StatelessWidget {
   static const String path = "lib/src/pages/food/avocado.dart";
