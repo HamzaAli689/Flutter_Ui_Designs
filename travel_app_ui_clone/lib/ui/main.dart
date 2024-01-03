be42c0b8b65fafe8644001e252e273de981f3ef9
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-01-03 15:48:35 +0500

    delete and remove some extra code

---
+++ travel_app_ui_clone/lib/ui/main.dart
@@ -0,0 +1,29 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+import '../screens/home_screen.dart';
+
+
+void main (){
+  runApp(TravelUiClone());
+}
+
+class TravelUiClone extends StatelessWidget {
+  static const String path = 'lib/src/travelui_clone/main.dart';
+
+  const TravelUiClone({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Travel UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFF3EBACE),
+        scaffoldBackgroundColor: const Color(0xFFF3F5F7), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xFFD8ECF1)),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
