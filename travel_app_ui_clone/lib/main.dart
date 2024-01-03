fc7db7be6df3ca06d4066066a08d194b502db9d1
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-01-03 15:58:09 +0500

    make some changes but app not run

--- travel_app_ui_clone/lib/main.dart
+++ travel_app_ui_clone/lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:travel_app_ui_clone/ui/main.dart';
+import '../screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TravelUiClone());
 }
+class TravelUiClone extends StatelessWidget {
+  static const String path = 'lib/src/travelui_clone/main.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const TravelUiClone({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Travel UI',
       debugShowCheckedModeBanner: false,
-      home: TravelUiClone(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFF3EBACE),
+        scaffoldBackgroundColor: const Color(0xFFF3F5F7), colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xFFD8ECF1)),
+      ),
+      home: HomeScreen(),
     );
   }
 }
