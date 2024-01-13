aafcec9765477a7191ce4b0a140380ef1870ddb4
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-01-13 19:56:06 +0500

    run successfully

--- login_ui_special_flutter_app/lib/main.dart
+++ login_ui_special_flutter_app/lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:login_ui_special_flutter_app/screens/onboarding/onboarding_page.dart';
 
-void main()
- {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: OnBoardingPage(),
+    return FutureBuilder<void>(
+      future: run(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: OnBoardingPage(),
+          );
+        } else {
+          // You can return a loading indicator here if needed
+          return Container();
+        }
+      },
     );
+  }
+
+  Future<void> run() async {
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    WidgetsFlutterBinding.ensureInitialized();
   }
 }
