10059e39fdc3ee200525de1c86a0e85e6b6a59bd
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-01-16 21:08:57 +0500

    replace the code of main.dart

--- untitled2/lib/main.dart
+++ untitled2/lib/main.dart
@@ -1,125 +1,295 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+const linearColor = LinearGradient(
+    colors: [Color(0xFFC60447), Color(0xFFA40053)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const protectionMsgs = [
+  "3 year protection plan for custom PC Build with super fast services",
+  "2 year protection plan for Alienware Monitors with cheap fixings"
+];
 
-  // This widget is the root of your application.
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Shopping cart',
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: Container(
+          margin: EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              WhiteText(
+                text: "Shopping Cart",
+                size: 24.0,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              CartItems(
+                price: "3,499.99",
+                itemName: "PC - Custom Build",
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              CartItems(
+                price: "3,499.99",
+                itemName: "PC - Custom Build",
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              WhiteText(
+                text: "Protection Plans",
+                size: 24.0,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ProtectionSection(),
+              SizedBox(
+                height: 40.0,
+              ),
+              BottomPart(),
+            ],
+          ),
+        ));
   }
+}
+
+class WhiteText extends StatelessWidget {
+  final String text;
+  final double size;
+
+  const WhiteText({Key key, this.text, this.size}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+    return Text(text, style: TextStyle(color: Colors.white, fontSize: size));
+  }
+}
+
+class CartItems extends StatelessWidget {
+  final String price;
+  final String itemName;
+
+  const CartItems({Key key, this.price, this.itemName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: 100.0,
+          height: 80.0,
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              color: Color(0xFF1E1D1E),
+              borderRadius: BorderRadius.circular(10.0)),
+          child: Center(
+            child: Image(
+              image: AssetImage("assets/customPC.png"),
+            ),
+          ),
+        ),
+        Spacer(),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            WhiteText(
+              text: "\$$price",
+              size: 18,
+            ),
+            SizedBox(
+              height: 5.0,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+              itemName,
+              style: TextStyle(
+                color: Color(0xFF5F5E60),
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.right,
+            )
           ],
         ),
+        Spacer(),
+        Icon(
+          Icons.more_vert,
+          color: Color(0xFF5F5E60),
+        )
+      ],
+    );
+  }
+}
+
+class ProtectionSection extends StatelessWidget {
+  const ProtectionSection({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180.0,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          PlanCard(),
+          PlanCardGrey(),
+          PlanCardGrey(),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class PlanCard extends StatelessWidget {
+  const PlanCard({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 10.0),
+      padding: EdgeInsets.all(12.0),
+      width: 140.0,
+      decoration: BoxDecoration(
+          gradient: linearColor, borderRadius: BorderRadius.circular(10.0)),
+      child: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              protectionMsgs[0],
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.w200),
+            ),
+          ),
+          Spacer(),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                "\$99.99",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w200,
+                    fontSize: 16.0),
+                textAlign: TextAlign.left,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class PlanCardGrey extends StatelessWidget {
+  const PlanCardGrey({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 10.0),
+      padding: EdgeInsets.all(12.0),
+      width: 140.0,
+      decoration: BoxDecoration(
+          color: Color(0xFF1E1D1E), borderRadius: BorderRadius.circular(10.0)),
+      child: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              protectionMsgs[1],
+              style:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.w200),
+            ),
+          ),
+          Spacer(),
+          Align(
+              alignment: Alignment.bottomRight,
+              child: Text("Add".toUpperCase(),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 16.0))),
+        ],
+      ),
+    );
+  }
+}
+
+class LinearButton extends StatelessWidget {
+  const LinearButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 140.0,
+      height: 40.0,
+      decoration: BoxDecoration(
+          gradient: linearColor, borderRadius: BorderRadius.circular(10.0)),
+      child: Center(
+        child: WhiteText(text: "Next"),
+      ),
+    );
+  }
+}
+
+class BottomPart extends StatelessWidget {
+  const BottomPart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Estimated Total",
+              style: TextStyle(
+                color: Color(0xFF5F5E60),
+                fontSize: 12,
+              ),
+            ),
+            SizedBox(
+              height: 2.0,
+            ),
+            WhiteText(
+              text: "\$4,027.97",
+              size: 22.0,
+            )
+          ],
+        ),
+        LinearButton()
+      ],
     );
   }
 }
