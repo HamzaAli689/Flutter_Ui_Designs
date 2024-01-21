410aedb987ec0a7615c77fe1153111e579569061
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-01-21 20:41:54 +0500

    replace the code of main.dart

--- untitled2/lib/main.dart
+++ untitled2/lib/main.dart
@@ -1,28 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:untitled2/shopping_cart_ui/shopping_cart2/home_page.dart';
-import 'bloom_app_ui/bloom_app.dart';
-import 'news_app_ui/news_app.dart';
+import 'package:wallart/pages/welcome.dart';
 
-const linearColor = LinearGradient(
-    colors: [Color(0xFFC60447), Color(0xFFA40053)],
-    begin: Alignment.topLeft,
-    end: Alignment.bottomRight);
+void main() {
+  runApp(MyApp());
+}
 
-const protectionMsgs = [
-  "3 year protection plan for custom PC Build with super fast services",
-  "2 year protection plan for Alienware Monitors with cheap fixings"
-];
-
-void main() => runApp(MainApp());
-
-class MainApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News app',
-      home: BloomApp(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+            brightness: Brightness.light,
+            textTheme: TextTheme(title: TextStyle(color: Colors.white)),
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+            actionsIconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+          )),
+      initialRoute: "/",
+      routes: {'/': (context) => WelcomePage()},
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
+//////////////////////////////////////////////////////////////////////////////////
+// main data(import 'package:flutter/material.dart';
+// import 'package:untitled2/shopping_cart_ui/shopping_cart2/home_page.dart';
+// import 'bloom_app_ui/bloom_app.dart';
+// import 'news_app_ui/news_app.dart';
+//
+// const linearColor = LinearGradient(
+//     colors: [Color(0xFFC60447), Color(0xFFA40053)],
+//     begin: Alignment.topLeft,
+//     end: Alignment.bottomRight);
+//
+// const protectionMsgs = [
+//   "3 year protection plan for custom PC Build with super fast services",
+//   "2 year protection plan for Alienware Monitors with cheap fixings"
+// ];
+//
+// void main() => runApp(MainApp());
+//
+// class MainApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'News app',
+//       home: BloomApp(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// })
