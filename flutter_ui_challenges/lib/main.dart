b1d743c2f8baf4b8330fda15a40cbf4a7de6fb0a
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-01-22 20:53:28 +0500

    successfully run the project

--- flutter_ui_challenges/lib/main.dart
+++ flutter_ui_challenges/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IOS Unlock',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           primaryTextTheme: TextTheme(
