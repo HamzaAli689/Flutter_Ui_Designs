9b446de7caea26dd8c84af9b7a438fff56229b0f
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-02-02 20:35:30 +0500

    update the main.dart

--- flutter_awesome_pages_ui/lib/main.dart
+++ flutter_awesome_pages_ui/lib/main.dart
@@ -8,12 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
