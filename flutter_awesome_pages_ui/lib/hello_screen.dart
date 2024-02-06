819e59ef8ecf444aff9682a4222ff6cdd371c4b3
Hamza Ali <127171562+HamzaAli689@users.noreply.github.com>
2024-02-06 22:25:44 +0500

    make some changes

--- flutter_awesome_pages_ui/lib/hello_screen.dart
+++ flutter_awesome_pages_ui/lib/hello_screen.dart
@@ -11,12 +11,21 @@ class _MyScreenAppState extends State<MyScreenApp> {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
           title: Text("Hamza Ali",style: TextStyle(
             color: Colors.black,
           )),
+        ),
+        backgroundColor: Colors.greenAccent,
+        body: Expanded(
+          child: Column(
+            children: [
+              Container()
+            ],
+          ),
         ),
       ),
     );
